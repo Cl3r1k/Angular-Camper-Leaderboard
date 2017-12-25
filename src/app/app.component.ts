38bac23b295b97d4063fe0f428c0f2715ce09ff7
Cl3r1k <Cl3r1k2@gmail.com>
2017-12-25 18:54:01 +0600

    Basic business logic and UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { map } from 'rxjs/operators/map';
+
+class User {
+    username: string;
+    recent: number;
+    alltime: number;
+    img: string;
+
+    constructor(userName: string, recent: number, alltime: number, img: string) {
+        this.username = userName;
+        this.recent = recent;
+        this.alltime = alltime;
+        this.img = img;
+    }
+}
 
 @Component({
     selector: 'app-root',
@@ -7,7 +24,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-    constructor() {
+    API_URLS = ['https://fcctop100.herokuapp.com/api/fccusers/top/recent', 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime'];
+    usersList: User[] = [];
+
+    constructor(private _httpClient: HttpClient) {
         this.initApp();
     }
 
@@ -19,6 +39,19 @@ export class AppComponent implements OnInit {
     }
 
     initApp() {
-        //
+        this.getUserList(0);
+    }
+
+    getUserList(val: number) {
+        this._httpClient.get(this.API_URLS[val]).subscribe((result) => {
+            // console.log(result);
+
+            Object.keys(result).forEach(key => {
+                // console.log('item: ', result[key]);
+                this.usersList.push(new User(result[key].username, result[key].recent, result[key].alltime, result[key].img));
+            });
+
+            // console.log('users list: ', this.usersList);
+        });
     }
 }
