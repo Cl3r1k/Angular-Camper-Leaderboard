df8e413ebd5eeb7a47b5ba267406d7f8f876579b
Cl3r1k <Cl3r1k2@gmail.com>
2017-12-27 17:49:43 +0600

    Added loader, minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,14 +24,15 @@ class User {
 })
 export class AppComponent implements OnInit {
 
+    state: number;
+    dataLoaded = false;
     API_URLS = ['https://fcctop100.herokuapp.com/api/fccusers/top/recent', 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime'];
     usersList: User[] = [];
 
-    constructor(private _httpClient: HttpClient) {
-        this.initApp();
-    }
+    constructor(private _httpClient: HttpClient) { }
 
     ngOnInit() {
+        this.initApp();
         const el = document.getElementById('loader-wrapper');
         if (el) {
             el.classList.add('loaded');
@@ -40,18 +41,31 @@ export class AppComponent implements OnInit {
 
     initApp() {
         this.getUserList(0);
+        this.state = 1;
     }
 
     getUserList(val: number) {
         this._httpClient.get(this.API_URLS[val]).subscribe((result) => {
             // console.log(result);
+            this.usersList = [];
 
             Object.keys(result).forEach(key => {
                 // console.log('item: ', result[key]);
                 this.usersList.push(new User(result[key].username, result[key].recent, result[key].alltime, result[key].img));
             });
 
+            this.dataLoaded = true;
             // console.log('users list: ', this.usersList);
         });
+    }
+
+    loadMonthlyPoints() {
+        this.getUserList(0);
+        this.state = 1;
+    }
+
+    loadAllTimePoints() {
+        this.getUserList(1);
+        this.state = 2;
     }
 }
